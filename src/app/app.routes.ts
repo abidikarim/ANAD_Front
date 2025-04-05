e3a16307d87a0ae7dad73c3a02611b4d1f94855b
abidikarim <abidikarim97@yahoo.com>
2025-04-05 23:57:09 +0100

    Fix routing with vercel.json and build output

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ReglementComponent } from './reglement/reglement.component';
 import { AUTComponent } from './aut/aut.component';
 import { SignalementDopageComponent } from './signalement-dopage/signalement-dopage.component';
 import { OutilsComponent } from './outils/outils.component';
+import { ActualitesComponent } from './actualites/actualites.component';
 
 export const routes: Routes = [
     {
@@ -16,7 +17,8 @@ export const routes: Routes = [
             { path: 'reglement', component: ReglementComponent, title: 'Reglement' },
             { path: 'aut', component: AUTComponent, title: 'AUT' },
             { path: 'signalement-dopage', component: SignalementDopageComponent, title: 'Signalement Dopage' },
-            { path: 'outils', component: OutilsComponent, title: 'Outils' }
+            { path: 'outils', component: OutilsComponent, title: 'Outils' },
+            { path: 'actualites', component: ActualitesComponent, title: 'Actualites' }
         ]
     }
 ];
